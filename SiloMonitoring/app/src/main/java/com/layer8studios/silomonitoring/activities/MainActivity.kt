ae9708c913cba5946b7c85ff66b6b18e4288b60d
LT <keinplan.luis@gmail.com>
2022-07-16 17:32:13 +0200

    Added basic project

---
+++ SiloMonitoring/app/src/main/java/com/layer8studios/silomonitoring/activities/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.layer8studios.silomonitoring.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.layer8studios.silomonitoring.databinding.ActivityMainBinding
+
+
+class MainActivity
+    : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fab.setOnClickListener { view ->
+
+        }
+    }
+
+}
